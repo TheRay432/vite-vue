e081942d1c5b1bba8f5181470d41d6c336b9fbb9
ray(company) <rayzheng@hitrust.com.tw>
2024-03-12 10:37:46 +0800

    測試PR
    新增一個變數,測試PR用

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ const name = ref('');
 const test = ref(null);
 const arr = computed(() => [getRequiredValidate('test123123', inputValue.value)]);
 const arr2 = computed(() => [getRequiredValidate('test123123', inputValue2.value)]);
+const testPullRequest = ref(null);
 const store = useValidateStore();
 function testFunc(): void {
   store.setValidate();
